fb5b0834eb620cfac05172490c7e15e1c41ff36f
Neil Macneale <neil.macneale.v@gmail.com>
2020-01-03 18:48:43 -0800

    Can give kits to player

--- src/main/java/net/cubiness/kits/Kit.java
+++ src/main/java/net/cubiness/kits/Kit.java
@@ -4,13 +4,15 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Kit {
+  private final List<ItemStack> items = new ArrayList<>();
   private String name;
   private int power;
-  private List<ItemStack> items;
 
   public Kit(String name, FileConfiguration config) {
 
@@ -19,14 +21,15 @@ public class Kit {
   public Kit(String name, int power, Player from) {
     this.name = name;
     this.power = power;
-    items = Arrays.asList(from.getInventory().getContents());
+    items.addAll(Arrays.asList(from.getInventory().getContents()));
+    items.removeAll(Collections.singleton(null));
+    from.sendMessage(items + "");
   }
 
   public void save(FileConfiguration config) {
-
   }
 
   public void give(Player p) {
-
+    items.forEach(i -> p.getInventory().addItem(i));
   }
 }
